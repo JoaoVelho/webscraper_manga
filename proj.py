1f5e6e2c1f069633b89697c3ca59b2e2f8697b51
Joao Velho <joaovictor.rv39@gmail.com>
2020-07-18 18:57:41 -0300

    send email works

--- proj.py
+++ proj.py
@@ -1,6 +1,22 @@
+import os
 import time
+import smtplib, ssl
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+load_dotenv()
+context = ssl.create_default_context()
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = os.getenv('EMAIL')  # Enter your address
+receiver_email = os.getenv('RECEIVER')  # Enter receiver address
+password = os.getenv('PASSWORD')
+message = """\
+Subject: New A Trail of Blood Chapter
+
+There's a new chapter of A Trail of Blood available!!."""
 
 PATH = '/mnt/c/Users/ADM/downloads/chromedriver.exe'
 
@@ -16,6 +32,9 @@ driver.get(url)
 
 try:
   if driver.find_element_by_xpath('/html/body/div[1]/div[2]/img[1]'):
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+      server.login(sender_email, password)
+      server.sendmail(sender_email, receiver_email, message)
     with open('cap.txt', 'w') as file:
       cap += 1
       file.write(str(cap))
