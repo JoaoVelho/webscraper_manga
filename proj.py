efbec172772843dd334e82fafe408101d826279a
Joao Velho <joaovictor.rv39@gmail.com>
2020-07-19 23:30:47 -0300

    cleaned code

--- proj.py
+++ proj.py
@@ -23,14 +23,6 @@ Subject: New Boruto Chapter
 
 There's a new chapter of Boruto available!!."""
 )
-# messages[0] = """\
-# Subject: New A Trail of Blood Chapter
-
-# There's a new chapter of A Trail of Blood available!!."""
-# messages[1] = """\
-# Subject: New Boruto Chapter
-
-# There's a new chapter of Boruto available!!."""
 
 PATH = 'C:\chromedriver.exe'
 
@@ -52,19 +44,6 @@ xpaths = [
 option = Options()
 option.headless = True
 driver = webdriver.Chrome(PATH)
-
-# driver.get(urlTrail)
-
-# try:
-#   if driver.find_element_by_xpath('/html/body/div[1]/div[2]/img[1]'):
-#     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#       server.login(sender_email, password)
-#       server.sendmail(sender_email, receiver_email, messageTrail)
-#     with open('cap.txt', 'w') as file:
-#       capTrail += 1
-#       file.write(str(cap))
-# except:
-#   print('Not found Trail')
 
 for i in range(0, len(urls)):
 
