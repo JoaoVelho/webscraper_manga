723858eb95bf273567c42f2971d22940fcfceff5
Joao Velho <joaovictor.rv39@gmail.com>
2020-07-20 23:40:21 -0300

    bug fixed and email message added link

--- proj.py
+++ proj.py
@@ -8,6 +8,11 @@ from selenium.webdriver.chrome.options import Options
 load_dotenv()
 context = ssl.create_default_context()
 
+caps = [1, 1]
+with open('cap.txt', 'r') as file:
+  caps[0] = int(file.readline())
+  caps[1] = int(file.readline())
+
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = f"{os.getenv('EMAIL')}"  # Enter your address
@@ -17,33 +22,30 @@ messages = (
   """\
 Subject: New A Trail of Blood Chapter
 
-There's a new chapter of A Trail of Blood available!!.""",
+There's a new chapter of A Trail of Blood available!!.
+""" + f"https://mangajar.com/manga/a-trail-of-blood/chapter/{caps[0]}",
   """\
 Subject: New Boruto Chapter
 
-There's a new chapter of Boruto available!!."""
+There's a new chapter of Boruto available!!.
+""" + f"https://borutomangaread.com/manga/boruto-chapter-{caps[1]}/"
 )
 
 PATH = 'C:\chromedriver.exe'
 
-caps = [1, 1]
-with open('cap.txt', 'r') as file:
-  caps[0] = int(file.readline())
-  caps[1] = int(file.readline())
-
 urls = [
   f"https://mangajar.com/manga/a-trail-of-blood/chapter/{caps[0]}",
-  f"https://borutomangaread.com/manga/boruto-chapter-{caps[1]}/"
+  f"https://borutomangaread.com/manga/boruto-chapter-{caps[1] + 1}/"
 ]
 
 xpaths = [
   "/html/body/div[1]/div[2]/img[1]",
-  "//div[@class='separator']/a"
+  "//div[@class='separator']"
 ]
 
 option = Options()
 option.headless = True
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(PATH, options=option)
 
 for i in range(0, len(urls)):
 
